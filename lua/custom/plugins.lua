cf77f7f75fe2e67a2ae3bbdef2e02b895209ca7f
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2024-06-27 10:39:06 +0100

    Add files via upload

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,108 @@
+local plugins = {
+  
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+
+
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function ()
+      require("nvim-silicon").setup({
+        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34"
+      })
+    end
+
+  },
+
+
+  {
+    "ellisonleao/carbon-now.nvim",
+    lazy = true,
+    cmd = "CarbonNow",
+    opts = {
+      options = {
+         bg = "rgba(80,153,240,1)",
+        width = "680",
+        theme = "material",
+        drop_shadow = true,
+        drop_shadow_blur = "51px",
+        drop_shadow_offset_y = "14px",
+        font_size = "14px",
+        font_family = "Fira Code",
+        line_height = "152%",
+        padding_horizontal = "26px",
+        padding_vertical = "30px",
+        language = "auto",  
+
+      },
+    },
+  },
+
+
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    opts = {
+      handlers = {}
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+      }
+    }
+  }
+}
+return plugins
