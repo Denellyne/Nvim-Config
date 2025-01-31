a11bbc726e87b45fd1529cd6976b1f51c5b3d13a
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2025-01-31 23:42:00 +0000

    Update to Nvim config

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,6 +3,11 @@ local M = {}
 M.dap = {
   plugin = true,
   n = {
+    ["<leader>tg"] = {
+      "<cmd> Telescope live_grep <CR>",
+      "Search files inside project",
+    },
+
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
       "Add breakpoint at line",
