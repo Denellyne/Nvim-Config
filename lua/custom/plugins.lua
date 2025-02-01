5edd129db2656badf5fef2f03480dd528e10c5dc
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2025-02-01 23:26:12 +0000

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -5,20 +5,6 @@ local plugins = {
     lazy = false,
   },
 
-
-  {
-    "michaelrommel/nvim-silicon",
-    lazy = true,
-    cmd = "Silicon",
-    config = function ()
-      require("nvim-silicon").setup({
-        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34"
-      })
-    end
-
-  },
-
-
   {
     "ellisonleao/carbon-now.nvim",
     lazy = true,
