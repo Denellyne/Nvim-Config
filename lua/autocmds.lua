196eeafa999ff161a4baa56250150318131fc3d9
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2026-03-09 11:38:22 +0000

    Update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,15 +2,21 @@ require "nvchad.autocmds"
 
 -- Folds
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  callback = function()
-    -- check if treesitter has parser
-    if require("nvim-treesitter.parsers").has_parser() then
-      -- use treesitter folding
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  callback = function(args)
+    local buf = args.buf
+    local ft = vim.bo[buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    -- Safely check if a parser is installed and can be loaded for this buffer
+    local has_parser = lang and pcall(vim.treesitter.get_parser, buf, lang)
+
+    if has_parser then
+      -- use treesitter folding (must be local to the buffer!)
+      vim.opt_local.foldmethod = "expr"
+      vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     else
-      -- use alternative foldmethod
-      vim.opt.foldmethod = "syntax"
+      -- use alternative foldmethod (must be local to the buffer!)
+      vim.opt_local.foldmethod = "syntax"
     end
   end,
 })
